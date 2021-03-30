343e2dcc0675874abc4867b0709527d9d464767b
Anton Bonder <anton.bonder@hotmail.com>
2021-03-30 11:01:19 +0200

    test voor fxml

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+
+public class Main extends Application{
     public static void main(String[] args){
         System.out.println("test");
+        launch(args);
 
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("test.fxml"));
+
+        Scene scene = new Scene(root, 300, 275);
+
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
     }
 }
