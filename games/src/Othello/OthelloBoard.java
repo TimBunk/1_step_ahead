973630da87979d0e0ecfd32524eb095817de848a
BYoran <58174168+BYoran@users.noreply.github.com>
2021-03-31 12:18:19 +0200

    Update OthelloBoard.java

--- games/src/Othello/OthelloBoard.java
+++ games/src/Othello/OthelloBoard.java
@@ -7,8 +7,11 @@ import java.util.Arrays;
 
 public class OthelloBoard extends AbstractBoard {
 
+    private char[] oldBoard;
+    
     public void initializeBoard(int size) {
         super.initializeBoard(size);
+        oldBoard = new char[board.length];
         Arrays.fill(board, '.');
     }
 
@@ -66,7 +69,17 @@ public class OthelloBoard extends AbstractBoard {
 
     @Override
     public void placeMove(int move, char c) {
+        for (int i = 0; i < board.length; i++) {
+            oldBoard[i] = board[i];
+        }
         board[move] = c;
+        turnStones(move, c);
+    }
+
+    public void undoLastMove() {
+        for (int i = 0; i < board.length; i++) {
+            board[i] = oldBoard[i];
+        }
     }
 
     @Override
