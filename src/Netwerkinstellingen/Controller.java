c939f5e9508d781b050a2b75c05754d7e25efd42
Arjan <55742327+Arjan14@users.noreply.github.com>
2021-03-31 12:20:04 +0200

    Afbeeldingen in Mainscreen + werkende knop naar netwerkinstellingen en terug.

--- src/Netwerkinstellingen/Controller.java
+++ src/Netwerkinstellingen/Controller.java
@@ -13,6 +13,23 @@ import java.io.IOException;
 public class Controller {
     private Model model = new Model();
 
+    @FXML
+    public void naarMain(ActionEvent event) {
+        System.out.println("Naar mainscreen...");
+        Parent root;
+        try {
+            FXMLLoader loader=new FXMLLoader(getClass().getClassLoader().getResource("MainScreen/View.fxml"));
+            root = (Parent) loader.load();
 
+            Stage stage=new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
