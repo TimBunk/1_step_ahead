ca0cb2667cebf92869e4c53fe85ec20c0652bb54
idshendriks <idshendriks@hotmail.com>
2021-04-01 12:35:41 +0200

    bij input van niet-integer herhaalt de vraag om een geldige zet zich

--- games/src/Othello/OthelloPlayer.java
+++ games/src/Othello/OthelloPlayer.java
@@ -10,7 +10,7 @@ public class OthelloPlayer extends AbstractPlayer {
     @Override
     public int doMove(AbstractBoard board) {
         Scanner sc= new Scanner(System.in);
-        int move;
+        int move = -1; // Always invalid
         do {
             // Lees wat de player intypt
             int[] validMoves = board.findValidMoves(getCharacter());
@@ -23,8 +23,15 @@ public class OthelloPlayer extends AbstractPlayer {
                     System.out.print( "en " + validMoves[i] + ".\nVoer één van deze zetten in: ");
                 }
             }
-            move = sc.nextInt();
-            System.out.println(move);
+
+            String moveString = sc.next();
+            try{
+                move = Integer.parseInt(moveString);
+                System.out.println(move);
+            } catch (NumberFormatException ignored){
+
+            }
+
             // En check of de player een valid move kiest om te spelen anders vraag de speler nogmaals een nummer in te vullen
         } while (!board.isMoveValid(move, getCharacter()));
         return move;
