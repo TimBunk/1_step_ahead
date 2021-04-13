d9360578c207624350e6dd61934db5d2dcdc559f
TimBunk <timbunk12@outlook.com>
2021-04-13 16:23:56 +0200

    Een nieuwe test voor OthelloComputer2

--- src/Othello/OthelloTest.java
+++ src/Othello/OthelloTest.java
@@ -14,44 +14,34 @@ public class OthelloTest {
         long startTime = new Date().getTime();
         BufferedWriter myWriter = new BufferedWriter(new FileWriter("computer2_DATA.txt"));
 
-        int[][] bestComputers = {
-                {6, 6, 63},
-                {6, 5, 61 },
-                {5, 4, 60 },
-                {5, 5, 56},
-                {4, 3, 52},
-                {4, 5, 44},
-                {4, 5, 44},
-                {5, 7, 43},
-                {4, 7, 43},
-                {5, 6, 41}
-        };
 
 
-        //for (int i=10;i<15;i+=5) { // 1 Tim
-        //for (int i=15;i<20;i+=5) { // 2 Anton
-        //for (int i=20;i<25;i+=5) { // 3 Arjan
-        //for (int i=25;i<25;i+=5) { // 4 Ids
-        //for (int i=30;i<35;i+=5) { // 5 Bjorn
-            for (int j=10;j<=30;j+=5) {
-                OthelloComputer2 player1 = new OthelloComputer2(7, 10000, 1, i, j);
+        // 10*10 = 100 keere doorlopen
+        //for (int i=10;i<=15;i+=5) { Tim
+        //for (int i=20;i<=25;i+=5) { Ids
+        //for (int i=30;i<=35;i+=5) { Anton
+        //for (int i=40;i<=45;i+=5) { Arjan
+        //for (int i=50;i<=55;i+=5) { Bjorn
+            for (int j=10;j<=55;j+=5) {
+                OthelloComputer2 player1 = new OthelloComputer2(7, 30000, 1, i, j);
 
                 int score = 0;
-                for (int k=0;k<bestComputers.length;k++) {
-                    OthelloComputer2 player2 = new OthelloComputer2(7, 10000, 1, bestComputers[k][0], bestComputers[k][1]);
+                // 10*10 = 100 keer doorlopen
+                for (int k=10;k<=55;k+=5) { // Stability
+                    for (int l = 10; l <= 55; l += 5) { // Mobility
+                        OthelloComputer2 player2 = new OthelloComputer2(7, 30000, 1, k, l);
 
-                    Othello othello = new Othello(player1, player2, false);
-                    score += othello.start();
-                    othello = new Othello(player2, player1, false);
-                    score += (-1 * othello.start());
-
+                        Othello othello = new Othello(player1, player2, false);
+                        score += othello.start();
+                        othello = new Othello(player2, player1, false);
+                        score += (-1 * othello.start());
+                    }
                 }
 
                 myWriter.write("" + i + " " + j + " " + score + "\n");
                 myWriter.flush();
             }
         }
-
 
         long endTime = new Date().getTime();
         long timeElapsed = endTime - startTime;
